68e125fb2d0501be0f7d879fdd8e67bdba96e2dc
alyti <alyti@users.noreply.github.com>
2022-01-09 04:05:20 +0000

    deploy: 2cfdfd9d26362d59e7fa6f33ce1c384bbea551c6

--- code/examples/generic-systems.rs
+++ code/examples/generic-systems.rs
@@ -3,12 +3,6 @@ use bevy::prelude::*;
 // ANCHOR: cleanup
 use bevy::ecs::component::Component;
 
-/// Marker components to group entities for cleanup
-mod cleanup {
-    pub struct LevelUnload;
-    pub struct MenuExit;
-}
-
 fn cleanup_system<T: Component>(
     mut commands: Commands,
     q: Query<Entity, With<T>>,
@@ -20,6 +14,15 @@ fn cleanup_system<T: Component>(
 // ANCHOR_END: cleanup
 
 // ANCHOR: main
+/// Marker components to group entities for cleanup
+mod cleanup {
+    use bevy::prelude::*;
+    #[derive(Component)]
+    pub struct LevelUnload;
+    #[derive(Component)]
+    pub struct MenuClose;
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 enum AppState {
     MainMenu,
@@ -27,14 +30,37 @@ enum AppState {
 }
 
 fn main() {
-    App::build()
+    App::new()
         .add_plugins(DefaultPlugins)
         .add_state(AppState::MainMenu)
         // add the cleanup systems
         .add_system_set(SystemSet::on_exit(AppState::MainMenu)
-            .with_system(cleanup_system::<cleanup::MenuExit>.system()))
+            .with_system(cleanup_system::<cleanup::MenuClose>))
         .add_system_set(SystemSet::on_exit(AppState::InGame)
-            .with_system(cleanup_system::<cleanup::LevelUnload>.system()))
+            .with_system(cleanup_system::<cleanup::LevelUnload>))
         .run();
 }
 // ANCHOR_END: main
+
+#[allow(dead_code)]
+mod constgenerics {
+use bevy::prelude::*;
+
+// ANCHOR: const
+fn process_layer<const LAYER_ID: usize>(
+    // system params
+) {
+    // do something for this `LAYER_ID`
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(process_layer::<1>)
+        .add_system(process_layer::<2>)
+        .add_system(process_layer::<3>)
+        .run();
+}
+// ANCHOR_END: const
+}
+
