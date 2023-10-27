91ddd71ba6febd70e117a453d21cbf41f087f87e
alyti <alyti@users.noreply.github.com>
2023-10-27 09:19:11 +0000

    deploy: 2cfdfd9d26362d59e7fa6f33ce1c384bbea551c6

--- code/src/basics.rs
+++ code/src/basics.rs
@@ -369,73 +369,6 @@ fn reset_health(
 }
 // ANCHOR_END: sys-query-set
 
-fn example_transforms() {
-// ANCHOR: transform-init
-// To simply position something at specific coordinates
-let xf_pos567 = Transform::from_xyz(5.0, 6.0, 7.0);
-
-// To scale an object, making it twice as big in all dimensions
-let xf_scale = Transform::from_scale(Vec3::splat(2.0));
-
-// To rotate an object in 2D (Z-axis rotation) by 30°
-// (angles are in radians! must convert from degrees!)
-let xf_rot2d = Transform::from_rotation(Quat::from_rotation_z((30.0_f32).to_radians()));
-
-// 3D rotations can be complicated; explore the methods available on `Quat`
-
-// Simple 3D rotation by Euler-angles (X, Y, Z)
-let xf_rot2d = Transform::from_rotation(Quat::from_euler(
-    EulerRot::XYZ,
-    (20.0_f32).to_radians(),
-    (10.0_f32).to_radians(),
-    (30.0_f32).to_radians(),
-));
-
-// Everything:
-let xf = Transform::from_xyz(1.0, 2.0, 3.0)
-    .with_scale(Vec3::new(0.5, 0.5, 1.0))
-    .with_rotation(Quat::from_rotation_y(0.125 * std::f32::consts::PI));
-// ANCHOR_END: transform-init
-}
-
-#[derive(Component)]
-struct Balloon;
-
-// ANCHOR: transform-mut
-fn inflate_balloons(
-    mut query: Query<&mut Transform, With<Balloon>>,
-    keyboard: Res<Input<KeyCode>>,
-) {
-    // every time the Spacebar is pressed,
-    // make all the balloons in the game bigger by 25%
-    if keyboard.just_pressed(KeyCode::Space) {
-        for mut transform in query.iter_mut() {
-            transform.scale *= 1.25;
-        }
-    }
-}
-// ANCHOR_END: transform-mut
-
-// ANCHOR: spatialbundle
-fn spawn_special_entity(
-    mut commands: Commands,
-) {
-    // create an entity that does not use one of the common Bevy bundles,
-    // but still needs transforms and visibility
-    commands.spawn((
-        ComponentA,
-        ComponentB,
-        SpatialBundle {
-            transform: Transform::from_scale(Vec3::splat(3.0)),
-            visibility: Visibility {
-                is_visible: false,
-            },
-            ..Default::default()
-        },
-    ));
-}
-// ANCHOR_END: spatialbundle
-
 // ANCHOR: propagation
 fn spawn_toplevel_entity(
     mut commands: Commands,
@@ -1942,34 +1875,6 @@ fn main() {
 }
 
 #[allow(dead_code)]
-mod app18 {
-use super::*;
-// ANCHOR: globaltransform
-/// Print the up-to-date global coordinates of the player as of **this frame**.
-fn debug_globaltransform(
-    query: Query<&GlobalTransform, With<Player>>,
-) {
-    let gxf = query.single();
-    debug!("Player at: {:?}", gxf.translation());
-}
-
-fn main() {
-    // the label to use for ordering
-    use bevy::transform::TransformSystem;
-
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_system_to_stage(
-            CoreStage::PostUpdate,
-            debug_globaltransform
-                .after(TransformSystem::TransformPropagate)
-        )
-        .run();
-}
-// ANCHOR_END: globaltransform
-}
-
-#[allow(dead_code)]
 mod app19 {
 use super::*;
 // ANCHOR: computedvisibility
@@ -2033,54 +1938,4 @@ fn main() {
         .run();
 }
 // ANCHOR_END: asset-watch
-}
-
-/// REGISTER ALL SYSTEMS TO DETECT COMPILATION ERRORS!
-pub fn _main_all() {
-    App::new()
-        .add_startup_system(load_ui_font)
-        .add_startup_system(load_extra_assets)
-        .add_startup_system(spawn_gltf)
-        .add_startup_system(load_gltf)
-        .add_startup_system(spawn_gltf_objects)
-        .add_startup_system(use_gltf_things)
-        .add_startup_system(gltf_manual_entity)
-        .add_startup_system(setup_bomb_spawning)
-        .add_startup_system(spawn_special_entity)
-        .add_startup_system(spawn_toplevel_entity)
-        .add_startup_system(setup_map_hidden)
-        .add_system(detect_removed_res)
-        .add_system(check_res_added)
-        .add_system(check_res_changed)
-        .add_system(commands_catchall)
-        .add_system(query_entities)
-        .add_system(query_player)
-        .add_system(query_misc)
-        .add_system(debug_player_hp)
-        .add_system(debug_stats_change)
-        .add_system(debug_new_hostiles)
-        .add_system(debug_damage)
-        .add_system(check_zero_health)
-        .add_system(reset_health)
-        .add_system(my_system1)
-        .add_system(my_system2)
-        .add_system(spawn_things)
-        .add_system(close_menu)
-        .add_system(make_all_players_hostile)
-        .add_system(use_sprites)
-        .add_system(fixup_images)
-        .add_system(update_player_xp)
-        .add_system(camera_with_parent)
-        .add_system(process_squad_damage)
-        .add_system(add_material)
-        .add_system(load_gltf_things)
-        .add_system(spawn_my_stuff)
-        .add_system(explode_bombs)
-        .add_system(spawn_bombs)
-        .add_system(jump_duration)
-        .add_system(asteroids_fly)
-        .add_system(despawn_child)
-        .add_system(inflate_balloons)
-        .add_system(reveal_map)
-        .run();
 }
